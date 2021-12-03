1c1954897f8cf98f3500f12169013427b52278d5
Chongoloka <msukwamose1@gmail.com>
2021-12-03 23:47:50 +0200

    linked to Nasa API

--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,6 +1,31 @@
 package com.udacity.asteroidradar.main
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.udacity.asteroidradar.api.AsteroidApi
+import com.udacity.asteroidradar.api.parseAsteroidsJsonResult
+import kotlinx.coroutines.launch
+import org.json.JSONObject
+import java.lang.Exception
 
 class MainViewModel : ViewModel() {
+
+
+    init {
+        getData()
+    }
+    fun getData(){
+        viewModelScope.launch {
+            try {
+                val response = AsteroidApi.retrofitService.getAsteroids()
+                //Log.e("reponse", response)
+               Log.i("Astroids from API", parseAsteroidsJsonResult(JSONObject(response)).size.toString())
+
+            }catch (e: Exception){
+                Log.e("error", e.message.toString())
+            }
+
+        }
+    }
 }
